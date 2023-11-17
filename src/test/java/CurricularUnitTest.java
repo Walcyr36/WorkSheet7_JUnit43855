a2e303134ea970357e6e7a4c7c1f92fa61d70f51
Cleusio <cleu024@gmail.com>
2023-11-17 12:04:02 +0000

    Improvments

--- src/test/java/CurricularUnitTest.java
+++ src/test/java/CurricularUnitTest.java
@@ -3,8 +3,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CurricularUnitTest {
 
@@ -24,7 +24,7 @@ public class CurricularUnitTest {
 
 
     @ParameterizedTest
-    @CsvSource({"10.0, 7,7, 8.9"})
+    @CsvSource({"10.0, 15.0, 11.9"})
     void testAverageCU(double mark1, double mark2, double mark3) {
         double expectedAverage = (mark1 + mark2 + mark3) / 3.0;
         CurricularUnit cu = new CurricularUnit("Matematica", 3);
@@ -36,7 +36,7 @@ public class CurricularUnitTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"7.0, 6.0, 14.8"})
+    @CsvSource({"9.5, 16.0, 14.8"})
     void testIsAproved(double mark1, double mark2, double mark3) {
         CurricularUnit cu = new CurricularUnit("Matematica", 3);
         cu.insertMark(1, mark1);
@@ -46,6 +46,20 @@ public class CurricularUnitTest {
         assertTrue(CurricularUnit.isAproved(1));
         assertTrue(CurricularUnit.isAproved(2));
         assertTrue(CurricularUnit.isAproved(3));
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({"9.4, 6.0, 4.8"})
+    void testIsAproved2(double mark1, double mark2, double mark3) {
+        CurricularUnit cu = new CurricularUnit("Matematica", 3);
+        cu.insertMark(1, mark1);
+        cu.insertMark(2, mark2);
+        cu.insertMark(3, mark3);
+
+        assertFalse(CurricularUnit.isAproved(1));
+        assertFalse(CurricularUnit.isAproved(2));
+        assertFalse(CurricularUnit.isAproved(3));
 
     }
 }
